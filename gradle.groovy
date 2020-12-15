32836b5a6324102a90e0e28cd11b48e7709c8a86
dptamayo22 <dptamayo22@gmail.com>
2020-12-15 20:33:29 -0300

    agregando archivos groovy

--- gradle.groovy
+++ gradle.groovy
@@ -19,7 +19,7 @@ def call(){
    				sh 'curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing'
             }
             stage('nexus'){
-                nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+                nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
             }
         }
     }
