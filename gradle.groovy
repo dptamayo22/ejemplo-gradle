866fa296d1ff71dba4e19148f2ac1d7192827b59
dptamayo22 <dptamayo22@gmail.com>
2020-12-15 20:38:20 -0300

    agregando archivos groovy

--- gradle.groovy
+++ gradle.groovy
@@ -7,7 +7,7 @@ def call(){
             	sh 'gradle clean build'
             }
             stage('sonar') {
-                ef scannerHome = tool 'sonar';
+                def scannerHome = tool 'sonar';
     			withSonarQubeEnv('sonar') {
       				sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
     			}
@@ -18,7 +18,9 @@ def call(){
             stage('rest') {
    				sh 'curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing'
             }
-            
+            stage('nexus'){
+                nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+            }
         }
     }
 }
